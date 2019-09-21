c1e3212a13de2a80cc2bdb030d61c0d4dfd78a01
Max Harley <t94j0@users.noreply.github.com>
2019-09-21 12:22:15 -0400

    Update re2.c

--- reverse-engineering/RE2/re2.c
+++ reverse-engineering/RE2/re2.c
@@ -1,5 +1,33 @@
 char flag[] = "CUCTF{p4ck3d_v3ry_p00rly}";
 
 int main(void){
+    char flag[24];
+    flag[0] = 'C';
+    flag[1] = 'U';
+    flag[2] =  'C';
+    flag[3] =  'T';
+    flag[4] =  'F';
+    flag[5] =  '{';
+    flag[6] =  'p';
+    flag[7] =  '4';
+    flag[8] =  'c';
+    flag[9] =  'k';
+    flag[10] =  '3';
+    flag[11] =  'd';
+    flag[12] =  '_';
+    flag[13] =  'v';
+    flag[14] =  '3';
+    flag[15] =  'r';
+    flag[16] =  'y';
+    flag[17] =  '_';
+    flag[18] =  'p';
+    flag[19] =  '0';
+    flag[20] =  '0';
+    flag[21] =  'r';
+    flag[22] =  'l';
+    flag[23] =  'y';
+    flag[24] =  '}';
+
+
     return 0;
 }
